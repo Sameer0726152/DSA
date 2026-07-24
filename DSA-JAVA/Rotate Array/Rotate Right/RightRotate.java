2ea3a0ea411f3a1f87954aebca9fa6a93bc014ce
Sameer Talekar <sameertalekar2007@gmail.com>
2026-07-24 19:39:09 +0530

    Added various files for Practice

---
+++ DSA-JAVA/Rotate Array/Rotate Right/RightRotate.java
@@ -0,0 +1,15 @@
+
+public class RightRotate {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int last = arr[arr.length - 1];
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ DSA-JAVA/Zeros at End/ZeroEnd.java
@@ -0,0 +1,21 @@
+
+public class ZeroEnd {
+
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 0, 4, 6, 0};
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
