e1f107b468baeb2821b997c434983267e858998c
Sameer Talekar <sameertalekar2007@gmail.com>
2026-08-06 23:45:48 +0530

    Added all functions for Stack using Linked List

--- DSA-JAVA/Stack-/Linked List Stack/stackll.java
+++ DSA-JAVA/Stack-/Linked List Stack/stackll.java
@@ -15,17 +15,46 @@ public class stackll {
     static class Stack {
 
         Node top;
+        int size;
 
         void push(int value) {
             Node newnode = new Node(value);
             newnode.next = top;
             top = newnode;
+            size++;
+        }
+
+        int pop() {
+            if (top == null) {
+                System.out.println("Stack Underflow");
+                return -1;
+            }
+            int value = top.val;
+            top = top.next;
+            size--;
+            return value;
+        }
+
+        int peek() {
+            if (top == null) {
+                System.out.println("Stack Underflow");
+                return -1;
+            }
+            return top.val;
+        }
+
+        boolean isempty() {
+            return top == null;
+        }
+
+        int size() {
+            return size;
         }
 
         void display() {
             Node temp = top;
             while (temp != null) {
-                System.out.println(temp.val + " ");
+                System.out.print(temp.val + " ");
                 temp = temp.next;
             }
             System.out.println("");
@@ -36,6 +65,17 @@ public class stackll {
         Stack stack = new Stack();
         stack.push(10);
         stack.push(20);
+        stack.push(30);
         stack.display();
+
+        System.out.println(stack.pop());
+        stack.display();
+
+        stack.push(30);
+        System.out.println(stack.peek());
+        stack.display();
+
+        System.out.println(stack.isempty());
+        System.out.println(stack.size());
     }
 }
